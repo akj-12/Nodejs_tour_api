960bf0c0fbc1a2967b3bc2f748a70300ebaeb2f6
Amit Jha <Amit.jha1@publicissapient.com>
2022-10-19 21:42:02 +0530

    db connection

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 // read env var and write into node env
 dotenv.config({ path: './config.env' });
+
+/**
+ * Database connection
+ */
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log(`DATABASE Connected successfully  `));
 
 /**
  * server
