f4c618c91d65ec370d7b7d7334e6df1906cc0a11
Amit Jha <Amit.jha1@publicissapient.com>
2022-11-05 10:24:30 +0530

    error handler global

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const errorHandler = require('./middleware/errorHandler');
 const tourRoutes = require('./routes/tourRoutes');
 const userRoutes = require('./routes/userRoutes');
+const ErrorHandler = require('./utils/errorHandler');
 
 const app = express();
 
@@ -18,4 +20,16 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
 
+/**
+ * Route error handler
+ */
+app.all('*', (req, res, next) => {
+  //   const err = new Error(`Can't find ${req.originalUrl} on this server !`);
+  //   err.statusCode = 404;
+  //   err.status = 'fail';
+  next(new ErrorHandler(`Can't find ${req.originalUrl} on this server !`, 404));
+});
+
+// error handler middleware
+app.use(errorHandler);
 module.exports = app;
