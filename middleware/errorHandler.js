e4cf959bfd94c68b5f5530e09fc6faa530fd1315
Amit Jha <Amit.jha1@publicissapient.com>
2022-11-20 13:50:53 +0530

    prod vs development ierror added

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,9 +1,36 @@
-module.exports = (err, req, res, next) => {
-  console.log(err.stack);
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+const sendDevelopmentError = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    message: err.message
+    message: err.message,
+    stack: err.stack,
+    error: err
   });
+};
+
+const sendProductionError = (err, res) => {
+  // operational trusted error
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    // programming or other unknown error
+    console.log('ERROR', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'something went very wrong !'
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendDevelopmentError(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendProductionError(err, res);
+  }
 };
