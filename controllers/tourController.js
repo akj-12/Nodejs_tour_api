7ed460b77e968c8e392970ae5d3ef4de99ecdc4c
Amit Jha <Amit.jha1@publicissapient.com>
2023-04-11 10:46:32 +0530

    response code dor create changed

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -36,7 +36,8 @@ exports.getAllTours = asyncHandler(async (req, res, next) => {
 
 exports.createTour = asyncHandler(async (req, res, next) => {
   const newTour = await Tour.create(req.body);
-  res.status(200).json({
+
+  res.status(201).json({
     data: {
       status: 'created',
       message: 'tour is created',
